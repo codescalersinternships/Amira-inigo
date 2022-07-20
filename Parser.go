3cf77491ff248d961c9f415528d8733cac51d0e7
AmiraHisham20 <amirahisham.258@gmail.com>
2022-07-20 11:15:03 +0200

    done

--- Parser.go
+++ Parser.go
@@ -2,17 +2,16 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
 	"regexp"
 	"strings"
-	"unicode"
 )
 
-func Checking(result string) bool {
+func isValidSectionName(result string) bool {
 	regularExpression := regexp.MustCompile("[[a-z|A-Z]+]")
 	if regularExpression.MatchString(result) {
-		
 		return true
 
 	}
@@ -21,55 +20,122 @@ func Checking(result string) bool {
 
 }
 
-type Inforamation struct {
+type Parser struct {
 	Data map[string]map[string]string
-
-	Comments [10]string
 }
 
-func Search(dictionary map[string]map[string]string, section string, key string) string {
-	return dictionary[section][key]
-}
+func (p *Parser) SetValues(section, key, value string) {
+	if _, ok := p.Data[section]; !ok {
+		p.SetSections(section)
 
-func LoadFromFile(name string, info Inforamation) {
-	file, ferr := os.Open(name)
-	if ferr != nil {
-		panic(ferr)
 	}
 
-	scanner := bufio.NewScanner(file)
-	LoadFromString(scanner, info)
+	p.Data[section][key] = value
 }
 
-func LoadFromString(scanner *bufio.Scanner, info Inforamation) {
+func (p *Parser) GetSectionNames() []string {
+
+	keys := make([]string, 0, len(p.Data))
+	for k := range p.Data {
+		keys = append(keys, k)
+
+	}
+
+	fmt.Println("sections are :", keys)
+	return keys
+}
+
+func (p *Parser) Get(section string, key string) (string, error) {
+	if _, ok := p.Data[section]; !ok {
+		return "", errors.New("No section with this name")
+	}
+	if _, ok := p.Data[section][key]; !ok {
+		return "", errors.New("No key with this name")
+	}
+
+	return p.Data[section][key], nil
+}
+func (p *Parser) SearchSection(section string) (err error) {
+	_, ok := p.Data[section]
+	if !ok {
+
+		return errors.New("No section with this name")
+	}
+	return nil
+}
+func (p *Parser) SetSections(section string) {
+	_, ok := p.Data[section]
+	if !ok {
+		fmt.Println("setsections1")
+
+		p.Data[section] = make(map[string]string)
+	}
+	fmt.Println("setsections")
+}
+
+func (p *Parser) SaveToFile(name string, dictionary map[string]map[string]string) (err error) {
+
+	file, ferr := os.Create(name)
+	defer file.Close()
+	if ferr != nil {
+		return errors.New("can't open file with this name")
+	}
+	for k := range dictionary {
+		_, err := file.WriteString(k + "\n")
+		for key, value := range dictionary[k] {
+			file.WriteString(key + " = " + value + "\n")
+
+		}
+		file.WriteString("\n")
+		if err != nil {
+
+			return errors.New("can't open file with this name")
+
+		}
+
+	}
+	return nil
+}
+func (p *Parser) LoadFromFile(name string) error {
+	f, ferr := os.ReadFile(name)
+	fmt.Println("openedfile")
+	if ferr != nil {
+		return errors.New("can't open the file with this name")
+	}
+	content := string(f)
+
+	return p.LoadFromString(string(content))
+}
+
+func (p *Parser) LoadFromString(content string) (err error) {
+
+	err = p.Parse(content)
+	return err
+	
+}
+
+func (p *Parser) Parse(content string) (err error) {
+	scanner := bufio.NewScanner(strings.NewReader(content))
 
 	var key string
 	var value string
-	d := make(map[string]map[string]string)
-
 	var section string
+	SectionFlag := false
 
-	length := 0
 	for scanner.Scan() {
 		line := scanner.Text()
 		items := strings.Split(line, " ")
-
+		fmt.Println((items[0]))
 		if items[0] == ";" {
 
-			for i := 0; i < len(items); i++ {
-				info.Comments[length] = line
-			}
-			length++
+			continue
 
-		} else if Checking(items[0]) {
+		} else if isValidSectionName(items[0]) {
 			section = items[0]
-			d[section] = make(map[string]string)
+			p.SetSections(section)
+			SectionFlag = true
 
-		} else if len(items) == 1 && items[0] == " " {
-			section = " "
-
-		} else {
-
+		} else if SectionFlag == true {
 			split_equal := strings.Split(line, "=")
 
 			if len(split_equal) == 2 {
@@ -77,74 +143,37 @@ func LoadFromString(scanner *bufio.Scanner, info Inforamation) {
 				key = split_equal[0]
 				value = split_equal[1]
 
-				d[section][key] = value
+				p.SetValues(section, key, value)
+
+			} else {
+				return errors.New("more than one value")
+			}
+
+		} else if len(items) == 1 {
+			if items[0] == " " {
 
 			}
 
+		} else {
+
+			return errors.New("Invalid input Name")
 		}
 
 	}
-	info.Data = d
-	fmt.Println(info.Data)
-
-}
-func check_name(name string) bool {
-	regularExpression := regexp.MustCompile("[a-z|A-Z]+")
-	if regularExpression.MatchString(name) {
-		return true
-
-	}
-	return false
+	p.SaveToFile("name.txt", p.Data)
+	return err
 }
 
 
-func check_port(port string) bool {
-	for _, c := range port {
-		if !unicode.IsDigit(c) {
-			return false
-		}
-	}
-	return true
-}
-func check_FileName(name string) bool {
 
-	regularExpression := regexp.MustCompile("[.].[txt|dat]")
-	if regularExpression.MatchString(name) {
-		return true
 
-	}
-	return false
-}
-
-func check_server(server string) bool {
-
-	regularExpression := regexp.MustCompile("[0-9]+[.][0-9]+[.][0-9]+[.][0-9]+")
-	if regularExpression.MatchString(server) {
-		return true
-
-	}
-	return false
-}
-
-func check_org(org string) bool {
-
-	regularExpression := regexp.MustCompile("[a-z|A-Z]+[.]")
-	if regularExpression.MatchString(org) {
-		return true
-
-	}
-	return false
-}
 
 func main() {
-
-	
-	var info Inforamation
+	Data := make(map[string]map[string]string)
+	var info Parser
 	var name = "text.INI"
-	LoadFromFile(name, info)
-
-	
+	info.LoadFromFile(name)
+	fmt.Println("printed from main", Data)
+	info.SaveToFile("name.txt", info.Data)
 
 }
-
-
